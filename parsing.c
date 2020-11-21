c47890eca6cf43c1114d420343922906702976af
Derek Briggs <contact@derekbriggs.xyz>
2020-11-21 12:50:19 -0500

    add basic Q-expression reading

--- parsing.c
+++ parsing.c
@@ -25,6 +25,7 @@ lval* lval_num(long x);
 lval* lval_err(char* m);
 lval* lval_sym(char* s);
 lval* lval_sexpr(void); 
+lval* lval_qexpr(void); 
 lval* lval_read_num(mpc_ast_t* t);
 lval* lval_read(mpc_ast_t* t);
 lval* lval_add(lval* v, lval* x);
@@ -37,13 +38,14 @@ void lval_println(lval* v);
 void lval_del(lval* v);
 
 
-enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR};
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 int main(int argc, char** argv) {
 	mpc_parser_t* Number    = mpc_new("number");
 	mpc_parser_t* Symbol    = mpc_new("symbol");
 	mpc_parser_t* Sexpr      = mpc_new("sexpr");
+	mpc_parser_t* Qexpr      = mpc_new("qexpr");
 	mpc_parser_t* Expr      = mpc_new("expr");
 	mpc_parser_t* Lispy     = mpc_new("lispy");
 
@@ -52,10 +54,11 @@ int main(int argc, char** argv) {
 	  number   : /-?[0-9]+/ ;            \
 	  symbol   : '+' | '-' | '*' | '/' ; \
 	  sexpr    : '(' <expr>* ')' ;       \
-	  expr     : <number> | <symbol> | <sexpr> ; \
+	  qexpr    : '{' <expr>* '}' ;       \
+	  expr     : <number> | <symbol> | <sexpr> | <qexpr> ; \
 	  lispy    : /^/ <expr>* /$/ ; \
 	  ",
-	  Number, Symbol, Sexpr, Expr, Lispy);
+	  Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
 
 	puts("Lispy Version 0.0.0.0.1");
@@ -86,7 +89,7 @@ int main(int argc, char** argv) {
 		free(input);
 	}
 
-	mpc_cleanup(4, Number, Symbol, Sexpr, Expr, Lispy);
+	mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 	return 0;
 }
 
@@ -170,6 +173,15 @@ lval* lval_sexpr(void) {
 	return v;
 }
 
+//a pointer to a new empty Qexpr lval
+lval* lval_qexpr(void) {
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_QEXPR;
+	v->count = 0;
+	v->cell = NULL;
+	return v;
+}
+
 lval* lval_read_num(mpc_ast_t* t) {
 	errno = 0;
 	long x = strtol(t->contents, NULL, 10);
@@ -186,11 +198,14 @@ lval* lval_read(mpc_ast_t* t) {
 	lval* x = NULL;
 	if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
 	if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+	if (strstr(t->tag, "qexpr")) { x = lval_qexpr(); }
 
 	//fill this list with any valid expression contained within
 	for (int i = 0; i < t->children_num; i++) {
 		if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
 		if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+		if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+		if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
 		if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
 		x = lval_add(x, lval_read(t->children[i]));
 	}
@@ -282,6 +297,9 @@ void lval_print(lval* v) {
 		 case LVAL_SEXPR:
 			 lval_expr_print(v, '(', ')');
 			 break;
+		 case LVAL_QEXPR:
+			 lval_expr_print(v, '{', '}');
+			 break;
 	 }
 }
 
@@ -312,6 +330,8 @@ void lval_del(lval* v) {
 		case LVAL_ERR: free(v->err); break;
 		case LVAL_SYM: free(v->sym); break;
 
+		//if qexpr or sexpr then delete all elements in the expression
+		case LVAL_QEXPR:
 		case LVAL_SEXPR:
 		   for (int i = 0; i < v->count; i++) {
 			   lval_del(v->cell[i]);
